d6b252715a9fb170f2abb0c6ab3a25fb224780c9
Uwe L. Korn <uwe.korn@quantco.com>
2023-07-03 09:21:47 +0200

    Explicitly set Python3_INCLUDE_DIR

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,10 @@ sed -i '/CMAKE_CXX_STANDARD/d' CMakeLists.txt
 # Let us set the CXXFLAGS
 sed -i '/CMAKE_CXX_FLAGS_PYTHON/d' CMakeLists.txt
 
-cmake $CMAKE_ARGS -GNinja -B build
+cmake $CMAKE_ARGS \
+	-B build \
+	-DPython3_INCLUDE_DIR:PATH=$PREFIX/include/`ls $PREFIX/include | grep "python\|pypy"` \
+	-GNinja
 cmake --build build
 cmake --install build
 $PYTHON ./setup.py bdist_wheel
